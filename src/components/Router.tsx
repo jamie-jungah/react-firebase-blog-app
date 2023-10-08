810f91d03d3e342748718a3c0a91c961a85f6adc
jamie-jungah <wjddk718@gmail.com>
2023-10-08 22:06:41 +0900

    Add all UI layout

---
+++ src/components/Router.tsx
@@ -0,0 +1,27 @@
+import { Route, Routes, Navigate, Link } from "react-router-dom";
+import Home from "pages/home";
+import Posts from "pages/posts";
+import PostPage from "pages/posts/detail";
+import NewPost from "pages/posts/new";
+import EditPost from "pages/posts/edit";
+import ProfilePage from "pages/profile";
+import Login from "pages/login";
+import Signup from "pages/signup";
+
+export default function Router() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:id" element={<PostPage />} />
+        <Route path="/posts/new" element={<NewPost />} />
+        <Route path="/posts/edit/:id" element={<EditPost />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
+  );
+}
