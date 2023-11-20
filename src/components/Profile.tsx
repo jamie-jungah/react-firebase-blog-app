1ecfbd4e51ffd85dee087ee2c377a144a393cb42
jamie-jungah <wjddk718@gmail.com>
2023-11-20 17:49:10 +0900

    Add logout, Context API Provider with Firebase Auth

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,18 +1,34 @@
-import { Link } from "react-router-dom";
+import { getAuth, signOut } from 'firebase/auth';
+import { app } from '../firebase';
+import { toast } from 'react-toastify';
+import { useContext } from 'react';
+import AuthContext from 'context/AuthContext';
+
+const onSignOut = async () => {
+  try {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success('로그아웃 되었습니다.');
+  } catch (error: any) {
+    console.log(error);
+    toast.error(error?.code);
+  }
+};
 
 export default function Profile() {
+  const { user } = useContext(AuthContext);
   return (
-    <div className="profile__box">
-      <div className="flex__box-lg">
-        <div className="profile__image" />
+    <div className='profile__box'>
+      <div className='flex__box-lg'>
+        <div className='profile__image' />
         <div>
-          <div className="profile__email">test@test.com</div>
-          <div className="profile__name">최정아</div>
+          <div className='profile__email'>{user?.email}</div>
+          <div className='profile__name'>{user?.displayName || ''}</div>
         </div>
       </div>
-      <Link to="/" className="profile__logout">
+      <div role='presentation' className='profile__logout' onClick={onSignOut}>
         로그아웃
-      </Link>
+      </div>
     </div>
   );
 }
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 import { app } from '../firebase';
@@ -10,6 +10,7 @@ export default function SignupForm() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [passwordConfirm, setPasswordConfirm] = useState<string>('');
+  const navigate = useNavigate();
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,6 +19,7 @@ export default function SignupForm() {
       await createUserWithEmailAndPassword(auth, email, password);
 
       toast.success('회원가입이 완료되었습니다.');
+      navigate('/');
     } catch (error: any) {
       console.log(error);
       toast.error(error?.code);
