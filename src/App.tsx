efd09b353138392a82455998770bbc45b6e6ed51
jamie-jungah <wjddk718@gmail.com>
2023-11-28 22:05:58 +0900

    Add light / dark mode feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { app } from 'firebaseApp';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
@@ -8,7 +8,10 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Loader from 'components/Loader';
 
+import ThemeContext from 'context/ThemeContext';
+
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
 
   // auth를 체크하기 전에 (initialize 전) loader를 띄워주는 용도
@@ -30,10 +33,10 @@ function App() {
     });
   }, [auth]);
   return (
-    <>
+    <div className={context.theme === 'light' ? 'white' : 'dark'}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   );
 }
 
